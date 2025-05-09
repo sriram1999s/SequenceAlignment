32e90f25bbde42dd8d3ec09bf16e6dd7bc4d8593
Chirag Srinivas <56649805+chiragsrinivas72@users.noreply.github.com>
2025-05-09 01:44:15 -0700

    added graph plots

--- plot_script.py
+++ plot_script.py
@@ -4,24 +4,29 @@ import matplotlib.pyplot as plt
 basic_path = "outputs/basic/"
 efficient_path = "outputs/efficient/"
 
-
 def get_last_two_floats(file_path):
     with open(file_path, "r") as f:
         lines = f.read().strip().split("\n")
         return float(lines[-2]), float(lines[-1])
+
+def get_time_and_mem_from_file(file_path):
+    with open(file_path,"r") as f:
+        lines = f.read().strip().split("\n")
+        return (float(lines[3]), float(lines[4]))
 
 if __name__ == '__main__':
     basic_time_values = []
     basic_mem_values = []
     efficient_time_values = []
     efficient_mem_values = []
+    total_str_lens_values = [16, 64,128,256,384, 512,768,1024, 1280, 1536, 2048, 2560, 3072, 3584, 3968]
 
     for i in range(1, 16):
         basic_file = basic_path + str(i) + '.txt'
         efficient_file = efficient_path + str(i) + '.txt'
 
-        basic_time, basic_mem = get_last_two_floats(basic_file)
-        efficient_time, efficient_mem = get_last_two_floats(efficient_file)
+        basic_time, basic_mem = get_time_and_mem_from_file(basic_file)
+        efficient_time, efficient_mem = get_time_and_mem_from_file(efficient_file)
 
         basic_time_values.append(basic_time)
         basic_mem_values.append(basic_mem)
@@ -29,25 +34,25 @@ if __name__ == '__main__':
         efficient_mem_values.append(efficient_mem)
 
     # Plot time comparison
-    plt.figure(figsize=(10, 5))
-    plt.plot(basic_time_values, label='Basic Time', marker='o')
-    plt.plot(efficient_time_values, label='Efficient Time', marker='o')
-    plt.xlabel("Test Case (1 to 15)")
+    plt.figure(figsize=(10,5))
+    plt.plot(total_str_lens_values, basic_time_values,   'o-', label='Basic Time')
+    plt.plot(total_str_lens_values, efficient_time_values,'o-', label='Efficient Time')
+    plt.xlabel("Total String Length")
     plt.ylabel("Time")
-    plt.title("Time Comparison")
+    plt.title("Time vs. Total String Length")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("time_comparison.png")
+    plt.savefig("time_vs_length.png")
 
     # Plot memory comparison
     plt.figure(figsize=(10, 5))
-    plt.plot(basic_mem_values, label='Basic Memory', marker='o')
-    plt.plot(efficient_mem_values, label='Efficient Memory', marker='o')
-    plt.xlabel("Test Case (1 to 15)")
+    plt.plot(total_str_lens_values,basic_mem_values, 'o-', label='Basic Time')
+    plt.plot(total_str_lens_values,efficient_mem_values, 'o-', label='Efficient Time')
+    plt.xlabel("Total String Length")
     plt.ylabel("Memory")
-    plt.title("Memory Comparison")
+    plt.title("Memory vs. Total String Length")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("memory_comparison.png")
+    plt.savefig("memory_vs_length.png")
